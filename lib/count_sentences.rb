6b833b4979fbb71a3398dab58f39b63491d9477f
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2019-12-11 21:47:36 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+     if self.end_with?(".") then  true 
+     else false 
+     end 
+  end 
+  
 
   def question?
-
+    if self.end_with?("?") then  true 
+    else false 
+    end 
+  
   end
 
   def exclamation?
+    if self.end_with?("!") then  true 
+    else false 
+    end 
 
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
